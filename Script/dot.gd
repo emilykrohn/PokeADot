cd6d9a41d6494db95027faf4cb42c0341600cbdf
Emily Krohn <emilykrohn2@gmail.com>
2024-02-11 20:39:21 -0800

    Dot disappears when clicked

---
+++ Script/dot.gd
@@ -0,0 +1,14 @@
+extends StaticBody2D
+
+var can_click := false
+
+func _input(event):
+	if event is InputEventMouseButton and can_click:
+		queue_free()
+
+func _on_mouse_entered():
+	can_click = true
+	print("enter")
+
+func _on_mouse_exited():
+	can_click = false
