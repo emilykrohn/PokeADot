db96c857feeefaa109e070e8d14ea2d9178cea89
Emily Krohn <emilykrohn2@gmail.com>
2024-02-11 19:47:48 -0800

    Create dot scene

--- Script/player.gd
+++ Script/player.gd
@@ -5,5 +5,3 @@ func _process(delta):
 	if mouse_position.x >= 0 and mouse_position.x <= 1152 and mouse_position.y >= 0 and mouse_position.y <= 648:
 		position = get_viewport().get_mouse_position()
 		move_and_slide()
-
-
