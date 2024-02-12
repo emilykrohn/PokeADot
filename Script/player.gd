63b2d98c41e7cc72b39f077718de5cb9cd113dfe
Emily Krohn <emilykrohn2@gmail.com>
2024-02-11 19:44:04 -0800

    Keep player cutout within screen borders

--- Script/player.gd
+++ Script/player.gd
@@ -1,4 +1,9 @@
-extends Area2D
+extends CharacterBody2D
 
 func _process(delta):
-	position = get_viewport().get_mouse_position()
+	var mouse_position = get_viewport().get_mouse_position()
+	if mouse_position.x >= 0 and mouse_position.x <= 1152 and mouse_position.y >= 0 and mouse_position.y <= 648:
+		position = get_viewport().get_mouse_position()
+		move_and_slide()
+
+
